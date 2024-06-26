118aa8204b48b3d0e1065ad89cf21019a8313e77
Kangyoongu <rkas7890@gmail.com>
2024-06-27 08:52:20 +0900

    end

--- Gang/Gang/GameLogic.h
+++ Gang/Gang/GameLogic.h
@@ -1,6 +1,30 @@
 #pragma once
 #include<vector>
+typedef struct _tagpos
+{
+	int x;
+	int y;
+	bool operator ==(const _tagpos& _pos)
+	{
+		if (x == _pos.x && y == _pos.y)
+			return true;
+		else
+			return false;
+	}
 
+}POS, * PPOS;
+
+typedef struct _tagplayer
+{
+	POS tPos; // 위치
+	bool beforeIsBlock; // 벽밀기
+	int jumpHeight;
+	int currentJump;
+}PLAYER, * PPLAYER;
+enum class OBJ_TYPE
+{
+	AIR = '0', BLOCK, STAR, SPINE, JUMP, ICE
+};
 const int MAP_WIDTH = 21;
 const int MAP_HEIGHT = 20;
 void Init();
