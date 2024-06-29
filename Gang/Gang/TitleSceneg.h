7518b2d9c525986db4d24f782142a75604c26a34
Kangyoongu <rkas7890@gmail.com>
2024-06-29 11:51:57 +0900

    fsasf

---
+++ Gang/Gang/TitleSceneg.h
@@ -0,0 +1,22 @@
+#ifndef TITLESCENE_H
+#define TITLESCENE_H
+
+void TitleRender();
+bool Title();
+
+enum class MENU
+{
+    FIRST, START, QUIT, END
+};
+
+MENU MenuRender();
+void InfoRender();
+
+enum class KEY
+{
+    NONE, UP, DOWN, SPACE
+};
+
+KEY KeyController();
+
+#endif // TITLESCENE_H
